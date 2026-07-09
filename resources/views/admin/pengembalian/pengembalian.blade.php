2c8e1ee9e13e1804a1d73ef2a1a63dabbe636f72
Daniel Anju Adinov Batubara <danielbatubara813@gmail.com>
2026-07-09 19:15:50 +0700

    menghapus tombol edit di halaman pengembalian

--- resources/views/admin/pengembalian/pengembalian.blade.php
+++ resources/views/admin/pengembalian/pengembalian.blade.php
@@ -93,7 +93,6 @@
                             <th class="px-6 py-3">Tanggal Kembali</th>
                             <th class="px-6 py-3">Denda</th>
                             <th class="px-6 py-3">Status</th>
-                            <th class="px-6 py-3">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -141,17 +140,6 @@
                                     <span class="px-3 py-1 rounded-full text-xs font-semibold {{ $statusClass }}">
                                         {{ $item->peminjaman->status }}
                                     </span>
-                                </td>
-                                <td class="px-6 py-4">
-                                    <button
-                                        class="inline-flex h-8 w-8 items-center justify-center rounded-md bg-yellow-300 text-black hover:bg-yellow-400 transition"
-                                        aria-label="Edit">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-edit-2">
-                                            <path d="m17 3 4 4L7 21H3v-4L17 3z" />
-                                        </svg>
-                                    </button>
                                 </td>
                             </tr>
                         @endforeach
